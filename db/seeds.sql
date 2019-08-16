910cee8e54ac60767a61e0317cce4d50c9c6aa7b
Andy <astevenson6@yahoo.com>
2019-08-16 13:25:01 -0700

    setup db and connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Western Bacon Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avacado Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
